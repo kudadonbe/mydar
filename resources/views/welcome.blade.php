cd0a0ab234d96dc7807b1376dd5570c4b39152d4
Hussain Shareef <hsaryfm@gmail.com>
2020-01-15 20:55:13 +0500

    New Updates with New Domain

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>hsaryfm.xyz</title>
+        <title>hsaryfm.tech</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    hsaryfm.xyz
+                    hsaryfm
                 </div>
 <!--
                 <div class="links">
